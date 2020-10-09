355dce46a2a3b88e10a405304a7c5b53fe0cb894
dstanfor350 <dale.stanford350@outlook.com>
2020-10-09 10:53:48 -0500

    Adding Unity Container injection types

--- UnityContainerDemo/Program.cs
+++ UnityContainerDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Unity;
+using Unity.Injection;
 
 namespace UnityContainerDemo
 {
@@ -6,7 +8,74 @@ namespace UnityContainerDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("UnityContainerDemo");
+
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<ICar, Ford>();// Map ICar with BMW 
+            container.RegisterType<ICar, Audi>("LuxuryCar");
+
+            // Registers Driver type
+            container.RegisterType<Driver>("LuxuryCarDriver",
+                new InjectionConstructor(container.Resolve<ICar>("LuxuryCar")));
+
+            //Resolves dependencies and returns the Driver object 
+            Driver drv1 = container.Resolve<Driver>();
+
+            drv1.RunCar();
+            drv1.RunCar();
+
+            Driver drv2 = container.Resolve<Driver>("LuxuryCarDriver");
+            drv2.RunCar();
+        }
+    }
+
+    public interface ICar
+    {
+        int Run();
+    }
+
+    public class BMW : ICar
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Ford : ICar
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Audi : ICar
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+
+    }
+    public class Driver
+    {
+        private ICar _car = null;
+
+        public Driver(ICar car)
+        {
+            _car = car;
+        }
+
+        public void RunCar()
+        {
+            Console.WriteLine("Running {0} - {1} mile ", _car.GetType().Name, _car.Run());
         }
     }
 }
