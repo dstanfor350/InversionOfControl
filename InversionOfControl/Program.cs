50985932d0366bc0d4f742b39165eb50eb3f6d6a
dstanfor350 <dale.stanford350@outlook.com>
2020-10-09 08:29:18 -0500

    Completed Inversion of Control project.  Starting UnityContainerDemo project

--- InversionOfControl/Program.cs
+++ InversionOfControl/Program.cs
@@ -6,7 +6,79 @@ namespace InversionOfControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("InversionOfControl");
+        }
+    }
+
+    public interface ICustomerDataAccess
+    {
+        string GetCustomerName(int id);
+    }
+
+    public class CustomerDataAccess : ICustomerDataAccess
+    {
+        public CustomerDataAccess()
+        {
+        }
+
+        public string GetCustomerName(int id)
+        {
+            return "Dummy Customer Name";
+        }
+    }
+
+    //public class DataAccess
+    //{
+    //    public DataAccess()
+    //    {
+    //    }
+
+    //    public string GetCustomerName(int id)
+    //    {
+    //        return "Dummy Customer Name"; // get it from DB in real app
+    //    }
+    //}
+
+    public class DataAccessFactory
+    {
+        public static ICustomerDataAccess GetCustomerDataAccessObj()
+        {
+            return new CustomerDataAccess();
+        }
+    }
+
+    public class CustomerService
+    {
+        CustomerBusinessLogic _customerBL;
+
+        public CustomerService()
+        {
+            _customerBL = new CustomerBusinessLogic(new CustomerDataAccess());
+        }
+
+        public string GetCustomerName(int id)
+        {
+            return _customerBL.ProcessCustomerData(id);
+        }
+    }
+
+    public class CustomerBusinessLogic
+    {
+        ICustomerDataAccess _dataAccess;
+
+        public CustomerBusinessLogic(ICustomerDataAccess custDataAccess)
+        {
+            _dataAccess = custDataAccess;
+        }
+
+        public CustomerBusinessLogic()
+        {
+            _dataAccess = new CustomerDataAccess();
+        }
+
+        public string ProcessCustomerData(int id)
+        {
+            return _dataAccess.GetCustomerName(id);
         }
     }
 }
